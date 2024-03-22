c1e65ecb32ce140bce9a566a7fd7ab159fe8ecf8
Patrycja Grzenkowicz <patrycja.grzenkowicz2@teb.edu.pl>
2024-03-22 15:58:33 +0100

    XD

---
+++ Assets/Scripts/AsteroidGenerator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidGenerator : MonoBehaviour
+{
+    //model zawieraj¹cy trzy kostki
+    GameObject model;
+    //wylosowana rotacja/s
+    Vector3 rotation = Vector3.one;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //przypisuje do zmiennej model obiekt-pojemnik zawieraj¹cy kostki
+        //bêd¹ce czêœci¹ modelu asteroidy
+        model = transform.Find("Model").gameObject;
+
+        //przygotuj generator liczb losowych
+        //Random r = new Random();
+        //nie robimy tego bo unity.random jest statyczne w przeciwienstwie do
+        // system.random
+
+        //iteruj przez czêœci modelu
+        foreach (Transform cube in model.transform)
+        {
+            //u¿yj wbudowanego random.rotation
+            cube.rotation = Random.rotation;
+
+            //losowa liczba
+            float scale = Random.Range(0.9f, 1.1f);
+
+            //przeskaluj
+            cube.localScale = new Vector3(scale, scale, scale);
+
+        }
+
+        //wylosuj jednorazowo rotacje/s naszej asteroidy
+        rotation.x = Random.value;
+        rotation.y = Random.value;
+        rotation.z = Random.value;
+        rotation *= Random.Range(10, 20);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //obróæ asteroidê (model) w wyznaczonym kierunku
+        model.transform.Rotate(rotation * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    //wspó³rzêdne gracza
+    Transform player;
+    //wysokoœæ kamery
+    public float cameraHeight = 10.0f;
+    //prêdkoœæ kamery - do u¿ytku dla smoothdamp
+    Vector3 cameraSpeed;
+    //szybkoœæ wyg³adzania ruchu kamery - dla smoothdamp
+    public float dampSpeed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pod³¹cz pozycjê gracza do lokalnej zmiennej korzystaj¹c z jego taga
+        //to nie jest zapisanie wartoœci jeden raz tylko referencja do obiektu
+        //to znaczy, ¿e player zawszê bêdzie zawiera³ aktualn¹ pozycjê gracza
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //oblicz docelow¹ pozycjê kamery
+        Vector3 targetPosition = player.position + Vector3.up * cameraHeight;
+
+        //p³ynnie przesuñ kamerê w kierunku gracza
+        //funkcja Vector3.Lerp
+        //p³ynnie przechodzi z pozycji pierwszego argumentu do pozycji drugiego w czasie trzeciego
+        //transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+
+        //smoothdamp dzia³a jak sprê¿yna staraj¹ca siê doci¹gn¹æ kamerê do was
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref cameraSpeed, dampSpeed);
+    }
+}
