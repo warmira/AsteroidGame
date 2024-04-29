dafb41d60bf06e812f425429e77e98fb43dc762d
warmira <127502346+warmira@users.noreply.github.com>
2024-04-29 20:33:47 +0200

    dynamic asteroid
     Author:    warmira <127502346+warmira@users.noreply.github.com>
    
     Author:    warmira <127502346+warmira@users.noreply.github.com>
     Date:      Mon Apr 22 20:33:47 2024 +0200

--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -15,10 +15,13 @@ public class AsteroidSpawner : MonoBehaviour
     float timeSinceSpawn;
 
     //odleg³oœ w jakiej spawnuj¹ siê asteroidy
-    public float spawnDistance = 15;
+    public float spawnDistance = 10;
 
     //odleg³oœæ pomiêdzy asteroidami
     public float safeDistance = 10;
+
+    //odstêp pomiedzy spawnem kolejnych asteroid
+    public float cooldown = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -33,12 +36,19 @@ public class AsteroidSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        SpawnAsteroid(staticAsteroid);
+        if (timeSinceSpawn > cooldown)
+        {
+            SpawnAsteroid(staticAsteroid);
+            timeSinceSpawn = 0;
+        }
+
 
         AsteroidCountControll();
+
+        timeSinceSpawn += Time.deltaTime;
     }
 
-    GameObject? SpawnAsteroid(GameObject prefab)
+    void SpawnAsteroid(GameObject prefab)
     {
         //generyczna funkcja sluzaca do wylosowania wspolrzednych i umieszczenia
         //w tym miejscu asteroidy z prefaba
@@ -54,19 +64,12 @@ public class AsteroidSpawner : MonoBehaviour
         randomPosition += player.position;
 
         //sprawdz czy miejsce jest wolne
-        //! oznacza "nie" czyli nie ma nic w promieniu 5 jednostek od miejsca randomPosition
+        //! oznacza "nie" czyli nie ma nic w promieniu jednostek od miejsca randomPosition
         if (!Physics.CheckSphere(randomPosition, safeDistance))
         {
             //stworz zmienn¹ asteroid, zespawnuj nowy asteroid korzystaj¹c z prefaba
             // w losowym miejscu, z rotacj¹ domyœln¹ (Quaternion.identity)
             GameObject asteroid = Instantiate(staticAsteroid, randomPosition, Quaternion.identity);
-
-            //zwróæ asteroidê jako wynik dzia³ania
-            return asteroid;
-        }
-        else
-        {
-            return null;
         }
 
     }
---
+++ Assets/Scripts/DynamicAsteroidController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DynamicAsteroidController : MonoBehaviour
+{
+    GameObject player;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //znajdz gracza na scenie
+        player = GameObject.FindWithTag("Player");
+        //obróæ siê przodem (z+) do gracza
+        transform.LookAt(player.transform.position);
+        //odniesienie do rigidbody asteroidy
+        rb = GetComponent<Rigidbody>();
+        //popchnij asteroide w kierunku gracza
+        rb.AddForce(transform.forward, ForceMode.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
